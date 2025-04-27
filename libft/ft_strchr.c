44da954f18dffde15aada674c326fc5e79f94289
André Marques <115092696+acmmarques@users.noreply.github.com>
2025-04-27 11:56:02 +0100

    fixed bug in strchr && added main

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -9,19 +9,33 @@
 /*   Updated: 2025/04/23 16:22:38 by andcardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdio.h>
 
 char	*ft_strchr(const char *s, int c)
 {
 	int	i;
 
 	i = 0;
+	c = (char)c;
 	while (s[i] != '\0')
 	{
 		if (s[i] == c)
 			return ((char *)&s[i]);
 		i++;
 	}
-	if (s[i] == '\0')
+	if (s[i] == '\0' && c == '\0')
 		return ((char *)&(s[i]));
 	return ((char *)0);
 }
+
+/*
+int	main (void)
+{
+	char *s = ft_strchr("hello", 'z');
+	char *d = strchr("hello", 'z');
+	//
+	printf("%p\n", (s));
+	printf("%p\n", (d));
+}
+ */
+
