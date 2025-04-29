825ffda11d11b27222b3e73aac26b9849f30f1ea
André Marques <115092696+acmmarques@users.noreply.github.com>
2025-04-29 16:35:47 +0100

    finished ft_strncmp

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,35 +6,37 @@
 /*   By: andcardo <andcardo@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 13:48:08 by andcardo          #+#    #+#             */
-/*   Updated: 2025/04/27 14:04:38 by andcardo         ###   ########.fr       */
+/*   Updated: 2025/04/29 16:35:27 by andcardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stddef.h>
 #include <stdio.h>
+#include <string.h>
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
+	if (n == 0)
+		return (0);
 	i = 0;
-	/*
-	*/
-	while (s1[i] == s2[i] && i < n && (s1[i] != '\0' || s2[i] != '\0'))
+	while (s1[i] == s2[i]
+		&& (i < n - 1)
+		&& (s1[i] != '\0' || s2[i] != '\0'))
 	{
-		printf("s1[%zu]=%c\t", i, s1[i]);
-		printf("s2[%zu]=%c\t", i, s2[i]);
-		printf("\n");
 		i++;
 	}
-	return (s1[i] - s2[i]);	
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 
+/*
 int main(void)
 {
-	char *one = "abcdefgh";
+	char *one = "zyxbcdefgh";
 	char *two = "abcdwxyz";
-	int a = ft_strncmp(one, two, 4);
+	int a = ft_strncmp(one, two, 0);
+	int b = strncmp(one, two, 0);
 	printf("dif->%d\n", a);
+	printf("dif->%d\n", b);
 }
-/*
  */
