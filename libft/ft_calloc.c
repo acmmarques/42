8eb8039ee4eaccff1c5c89ef6b7e945d28be13f3
André Marques <115092696+acmmarques@users.noreply.github.com>
2025-04-30 15:14:05 +0100

    finished calloc

---
+++ libft/ft_calloc.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: andcardo <andcardo@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/30 11:10:06 by andcardo          #+#    #+#             */
+/*   Updated: 2025/04/30 15:11:32 by andcardo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void	ft_bzero(void *s, size_t n);
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+	size_t	total_size;
+
+	if (size == 0 || count == 0)
+	{
+		ptr = malloc(1);
+		return (ptr);
+	}
+	total_size = count * size;
+	ptr = (void *)malloc(total_size);
+	if (ptr == NULL)
+		return (NULL);
+	ft_bzero(ptr, total_size);
+	return (ptr);
+}
+
+/*
+int	main(void)
+{
+	printf("Hello from main\n");
+	char *str = (char *)ft_calloc(30, 1);
+	int i = 0;
+	while (i < 30)
+	{
+		printf("p[%i] = %p\t", i, str);
+		printf("%i\n", *str);
+		i++;
+		str++;
+	}
+}
+ */
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: andcardo <andcardo@student.42lisboa.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 17:22:16 by andcardo          #+#    #+#             */
-/*   Updated: 2025/04/30 11:02:46 by andcardo         ###   ########.fr       */
+/*   Updated: 2025/04/30 14:43:48 by andcardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,5 +35,6 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
+void	*ft_calloc(size_t count, size_t size);
 
 #endif
